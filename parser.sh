2ed4062fb69b0e058fef2339ab5152321faafcf8
Adheer Araokar <adheer.araokar@dominodatalab.com>
2022-01-02 15:29:50 +0530

    Added default case for invalid flags.

--- parser.sh
+++ parser.sh
@@ -4,6 +4,7 @@ do
     case "${flag}" in
         d) date=${OPTARG};;
         f) filepath=${OPTARG};;
+        *) echo "Usage: $0 [-d -f]";;
     esac
 done
 python3 -m cookie_log_parser -d $date -f $filepath
